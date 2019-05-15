55cc40fac614fd9c5ae7eb9d8c3bdd01db4b0d81
kochol <cyrusgp@gmail.com>
2019-05-16 00:19:26 +0300

    Set the start block to 4340000

--- EtherChain/EtherChain/AppSettings.cs
+++ EtherChain/EtherChain/AppSettings.cs
@@ -4,7 +4,7 @@ namespace EtherChain
 {
     public static class AppSettings
     {
-        public static BigInteger StartBlock = 0; // 0 means start the block scan from latest block - 64.
+        public static BigInteger StartBlock = 4340000; // 0 means start the block scan from latest block - 64.
                                                 // Other values means start the block scan from this block number
                                                // Useful after publishing the production server.
         public static int BlockChunk = 10000; // How many block to get for processing each time.
