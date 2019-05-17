ab4d88741cecc98247bb9df4d4e3bfc0c24ee3dc
Hadi <hadi.robati@gmail.com>
2019-05-17 23:30:19 +0800

    Request bulk data

--- EtherChain/EtherChain/Controllers/AddressController.cs
+++ EtherChain/EtherChain/Controllers/AddressController.cs
@@ -1,6 +1,7 @@
 ﻿using EtherChain.Models;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace EtherChain.Controllers
 {
@@ -13,11 +14,45 @@ namespace EtherChain.Controllers
         [HttpGet("{add}/{id}")]
         public ActionResult<Address> Get(string id, string add)
         {
-            add = add.ToLower();
-            if (add.Substring(0, 2) == "0x")
-                add = add.Substring(2);
-
             return Program.db.GetAddress(id, add);
+        }
+
+        [HttpPost("getBulk")]
+        public ActionResult<ResBulkAddress> GetBulk([FromBody] ReqBulkAddress req)
+        {
+            if (req == null || req.addresses == null || req.addresses.Count == 0 || string.IsNullOrEmpty(req.coinName) )
+            {
+                return new ResBulkAddress
+                {
+                    success = false,
+                    Addresses = null,
+                };
+            }
+
+            ResBulkAddress res = new ResBulkAddress();
+
+            foreach (string addStr in req.addresses)
+            {
+                Address a = Program.db.GetAddress(addStr, req.coinName);
+                List<Transaction> txs = null;
+                if ( req.isGetTx && a.TrKeys.Count > 0)
+                {
+                    txs = new List<Transaction>();
+                    for( int i = 0; i < a.TrKeys.Count; i++)
+                    {
+                        txs.Add(Program.db.GetTransaction(a.TrKeys[i], req.coinName));
+                    }
+                }
+
+                res.Addresses.Add(new FullAddressInfo {
+                    address = a,
+                    transactions = txs,
+                });
+            }
+
+            res.success = true;
+
+            return res;
         }
     }
 }
